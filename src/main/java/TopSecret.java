27a1d83f0bba39614fc620d5aefcac7257e33235
felgizouli <falelgizouli@yahoo.com>
2026-02-10 18:21:16 -0500

    Fix indexing and argument handling in TopSecret

--- src/main/java/TopSecret.java
+++ src/main/java/TopSecret.java
@@ -9,25 +9,56 @@ public class TopSecret {
 
 
     public static void main(String[] args) {
-        if(args.length == 0){
+        if (args.length == 0) {
             List<String> fileNames = fileHandler.getAvailableFiles();
-            for(int i = 0; i < fileNames.size(); i++){
-                if(i<9){
-                    System.out.println("0"+(i+1)+" "+fileNames.get(i));//if the file index is a single digit number, add a zero to the front
-                }
-                else{
-                    System.out.println((i+1)+ " "+ fileNames.get(i));
+
+            for (int i = 0; i < fileNames.size(); i++) {
+                if (i < 9) {
+                    System.out.println("0" + (i + 1) + " " + fileNames.get(i));//if the file index is a single digit number, add a zero to the front
+                } else {
+                    System.out.println((i + 1) + " " + fileNames.get(i));
                 }
             }
-        }
-        else{
-            /*
-            Fal's part of the code, including command line UI and non empty args test case
-             */
-        }
+        } else {
+                if (args.length < 1) {
+                    System.out.println("Error: Invalid command");
+                    return;
+                }
+
+                String input = args[0];
+                int index = 0;
+
+                for (int i = 0; i < input.length(); i++) {
+                    char c = input.charAt(i);
+
+                    if (c < '0' || c > '9') {
+                        System.out.println("Error: File number must be numeric");
+                        return;
+                    }
+
+                    index = index * 10 + (c - '0');
+                }
 
 
-    }
+                List<String> fileNames = fileHandler.getAvailableFiles();
+
+                if (index < 0 || index >= fileNames.size()) {
+                    System.out.println("Error: File number out of range");
+                    return;
+                }
+
+                fileName = fileNames.get(index);
+
+                try {
+                    System.out.println(fileHandler.readFile(fileName));
+                } catch (Exception e) {
+                    System.out.println("Error: File not found or cannot be read");
+                }
+            }
+
+
+
+        }
 
 
     public static FileHandler getFileHandler() {
